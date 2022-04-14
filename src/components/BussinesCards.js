fa1c3f05a8d1bee04972b48b53a7ee2e1ceb9348
Byronodhiambo <byronjonnes25@gmail.com>
2022-04-14 08:55:58 +0300

    business card

--- src/components/BussinesCards.js
+++ src/components/BussinesCards.js
@@ -1,44 +1,128 @@
 import React from "react";
 import fredrick from "../images/fredrick.jpeg";
+import girish from "../images/Girish.jpg";
+import mitesh from "../images/mitesh.jpg";
 export default function BusinessCard() {
   return (
-    <div className="businessCard">
-      <img className="cardImage" src={fredrick} />
-      <div className="businessContent">
-        <div className="personalInfo">
-          <h3 className="info">Fredrick Maina</h3>
-          <h6 className="info">Accountant</h6>
-          <span className="info">
-            <button>
-              <i class="fa fa-envelope" aria-hidden="true"></i>Email
-            </button>
-            <button>
-              <i class="fa fa-linkedin-square" aria-hidden="true"></i>Linkedln
-            </button>
-          </span>
+    <div className="employees">
+      <div className="card">
+        <img className="cardImage" src={fredrick} />
+        <div className="cardContent">
+          <div className="personalInfo">
+            <h2 className="info">Fredrick Maina</h2>
+            <h6 className="info">Accountant</h6>
+            <span className="info">
+              <button>
+                <i class="fa fa-envelope" aria-hidden="true"></i>Email
+              </button>
+              <button>
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>Linkedln
+              </button>
+            </span>
+          </div>
+
+          <div className="bio">
+            <h3>About</h3>
+            <p>
+              Driven accountant with five years of experience supervising 100s
+              of tax filings. Responsible for managing Avid Corp's auditing and
+              tax filings.
+            </p>
+            <h3>Role</h3>
+            <p>
+              Performs accounting functions such as account analysis, auditing,
+              or financial statement analysis. Accountants can find employment
+              with an accounting firm or a large company with an internal
+              accounting department, or they can set up an individual practice.
+            </p>
+          </div>
         </div>
 
-        <div className="bio">
-          <h5>About</h5>
-          <p>
-            Driven accountant with five years of experience supervising 100s of
-            tax filings. Responsible for managing Avid Corp's auditing and tax
-            filings.
-          </p>
-          <h5>Role</h5>
-          <p>
-            Performs accounting functions such as account analysis, auditing, or
-            financial statement analysis. Accountants can find employment with
-            an accounting firm or a large company with an internal accounting
-            department, or they can set up an individual practice.
-          </p>
+        <div className="socialHandles">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
         </div>
       </div>
 
-      <div className="socialHandles">
-        <i class="fa fa-facebook" aria-hidden="true"></i>
-        <i class="fa fa-twitter" aria-hidden="true"></i>
-        <i class="fa fa-instagram" aria-hidden="true"></i>
+      <div className="card">
+        <img className="cardImage" src={girish} />
+        <div className="cardContent">
+          <div className="personalInfo">
+            <h2 className="info">Girish Karnad</h2>
+            <h6 className="info">C E O</h6>
+            <span className="info">
+              <button>
+                <i class="fa fa-envelope" aria-hidden="true"></i>Email
+              </button>
+              <button>
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>Linkedln
+              </button>
+            </span>
+          </div>
+
+          <div className="bio">
+            <h3>About</h3>
+            <p>
+              Driven accountant with five years of experience supervising 100s
+              of tax filings. Responsible for managing Avid Corp's auditing and
+              tax filings.
+            </p>
+            <h3>Role</h3>
+            <p>
+              Performs accounting functions such as account analysis, auditing,
+              or financial statement analysis. Accountants can find employment
+              with an accounting firm or a large company with an internal
+              accounting department, or they can set up an individual practice.
+            </p>
+          </div>
+        </div>
+
+        <div className="socialHandles">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </div>
+      </div>
+
+      <div className="card">
+        <img className="cardImage" src={mitesh} />
+        <div className="cardContent">
+          <div className="personalInfo">
+            <h2 className="info">Mitesh Patel</h2>
+            <h6 className="info">Manager</h6>
+            <span className="info">
+              <button>
+                <i class="fa fa-envelope" aria-hidden="true"></i>Email
+              </button>
+              <button>
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>Linkedln
+              </button>
+            </span>
+          </div>
+
+          <div className="bio">
+            <h3>About</h3>
+            <p>
+              Driven accountant with five years of experience supervising 100s
+              of tax filings. Responsible for managing Avid Corp's auditing and
+              tax filings.
+            </p>
+            <h3>Role</h3>
+            <p>
+              Performs accounting functions such as account analysis, auditing,
+              or financial statement analysis. Accountants can find employment
+              with an accounting firm or a large company with an internal
+              accounting department, or they can set up an individual practice.
+            </p>
+          </div>
+        </div>
+
+        <div className="socialHandles">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </div>
       </div>
     </div>
   );
