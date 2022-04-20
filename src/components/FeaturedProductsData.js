18a4d1fdae33cde69ab46e704e82323c80d706b7
Byronodhiambo <byronjonnes25@gmail.com>
2022-04-21 01:04:41 +0300

    featured products

---
+++ src/components/FeaturedProductsData.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    img: "images/REARRIGHTGASSHOCK.jpg",
+    rating: "5.0 (6) . Kenya",
+    desc: "The best quality Rear Right Gas Shock",
+    price: "Kshs. 3,500",
+  },
+  {
+    img: "images/prodimage2.jpg",
+    rating: "4.0 (15) . Kenya",
+    desc: "The best quality Air Filter",
+    price: "Kshs. 4,500",
+  },
+  {
+    img: "images/prodimage3.jpg",
+    rating: "4.6 (2) . Kenya",
+    desc: "The best quality Oil Filter",
+    price: "Kshs. 8,000",
+  },
+  {
+    img: "images/prodimage4.jpg",
+    rating: "3.9 (17) . Kenya",
+    desc: "The best quality Breake Pad & Shoe",
+    price: "Kshs. 3,400",
+  },
+  {
+    img: "images/prodimage5.jpg",
+    rating: "4.8 (9) . Kenya",
+    desc: "The best quality Condenser",
+    price: "Kshs. 8,000",
+  },
+  {
+    img: "images/prodimage6.jpg",
+    rating: "4.2 (10) . Kenya",
+    desc: "The best quality Radiator",
+    price: "Kshs. 7,000",
+  },
+  {
+    img: "images/prodimage7.jpg",
+    rating: "5.0 (14) . Kenya",
+    desc: "The best quality Gasket",
+    price: "Kshs. 1,300",
+  },
+  {
+    img: "images/prodimage8.jpg",
+    rating: "4.4 (13) . Kenya",
+    desc: "The best quality Shock Absober",
+    price: "Kshs. 2,800",
+  },
+  {
+    img: "images/prodimage9.jpg",
+    rating: "4.7 (5) . Kenya",
+    desc: "The best quality Fan & Motor",
+    price: "Kshs. 5,500",
+  },
+];
