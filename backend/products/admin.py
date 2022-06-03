b915eae711355cdc5219d9d949390c45f82e563d
Byronodhiambo <byronjonnes25@gmail.com>
2022-06-03 23:30:19 +0300

    register models

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Products, Category, Orders, Cart
+from django.contrib.auth.models import Group
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'desc', 'price', 'stock']
+    list_filter = ['category']
+    change_list_template
+
+admin.site.site_header = "Product admin"
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Orders)
+admin.site.register(Cart)
+
+admin.site.unregister(Group)
