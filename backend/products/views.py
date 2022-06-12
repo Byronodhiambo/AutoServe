e13fa2dcd183a3df991081edbb03493e2360b537
Byronodhiambo <byronjonnes25@gmail.com>
2022-06-12 20:33:14 +0300

    jwt

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,9 +1,11 @@
+from itertools import permutations
 from django.shortcuts import render
 from .models import Products, Category,Image
 from .serializers  import ProductSerializer, CategorySerializer, ImageSerializer
 # from rest_framework.viewsets import ReadOnlyModelViewset
 from rest_framework import viewsets
 from rest_flex_fields.views import FlexFieldsModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ class Products(viewsets.ReadOnlyModelViewSet):
 
     serializer_class = ProductSerializer
     queryset = Products.objects.all()
+    permutation_classes = [IsAuthenticated]
 
 # Uploading image via api
 class ImageViewSet(FlexFieldsModelViewSet):
