aad5d6c34307e7cbb4f3b4c0c65017ae76d4b5ea
Byronodhiambo <byronjonnes25@gmail.com>
2022-06-17 05:30:24 +0300

    jwt

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from products.views import Products, ImageViewSet
+from products.views import Products, Image
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -26,13 +26,14 @@ from rest_framework_simplejwt.views import (
 
 router = DefaultRouter()
 router.register(r'product', Products, basename='Products')
+router.register(r'image', Image, basename='Image')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('image/',  ImageViewSet.as_view(), name='image'),
 
 ]
 # To make Django development server serve media
--- backend/products/views.py
+++ backend/products/views.py
@@ -16,6 +16,7 @@ class Products(viewsets.ReadOnlyModelViewSet):
     permutation_classes = [IsAuthenticated]
 
 # Uploading image via api
-class ImageViewSet(FlexFieldsModelViewSet):
+class Image(FlexFieldsModelViewSet):
     serializer_class = ImageSerializer
     queryset = Image.objects.all()
+    permutation_classes = [IsAuthenticated]
